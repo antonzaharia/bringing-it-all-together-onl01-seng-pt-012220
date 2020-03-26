20b76fbd2d57b4a0e1549bd2d556d6aaee31f3ed
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-26 17:40:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
   end
   
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).first
     if !dog.empty?
       dog_array = dog[0]
       dog = Dog.new(dog_array[0], dog_array[1], dog_array[2])
